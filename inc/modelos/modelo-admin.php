a78964e5e4b886caf13937e0db402d0e7003c248
Juancaceres963 <dnijuancaceres@gmail.com>
2023-03-21 16:17:11 -0300

    Hashear passwords en modelo-admin.php

--- inc/modelos/modelo-admin.php
+++ inc/modelos/modelo-admin.php
@@ -1,3 +1,25 @@
 <?php 
 
-die(json_encode($_POST));
+$accion = $_POST['accion'];
+$password = $_POST['password'];
+$usuario = $_POST['usuario'];
+
+if($accion === 'crear'){
+    //Codigo para crear los administradores
+
+    //Hashear passwords
+    $opciones = array(
+        'cost' => 12
+    );
+    $hash_password = password_hash($password, PASSWORD_BCRYPT, $opciones);
+
+    $respuesta = array(
+        'pass' => $hash_password
+    );
+
+    echo json_encode($respuesta);
+}
+
+if($accion === 'login'){
+    //Codigo para loguear a los administradores
+}
