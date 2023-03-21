2d90337c293181a4e4bd0354078304e9d7b94731
Juancaceres963 <dnijuancaceres@gmail.com>
2023-03-21 16:04:45 -0300

    Conexion entre formulario.js y modelo-admin.php atravez de Ajax

--- inc/templates/footer.php
+++ inc/templates/footer.php
@@ -1,4 +1,3 @@
-<script src="./js/sweetalert2.all.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <?php
     $actual = obtenerPaginaActual();
--- js/formulario.js
+++ js/formulario.js
@@ -8,19 +8,40 @@ function validarRegistro(e) {
     e.preventDefault();
 
     var usuario = document.querySelector('#usuario').value,
-        password = document.querySelector('#password').value;
+        password = document.querySelector('#password').value,
+        tipo = document.querySelector('#tipo').value;
 
     if (usuario == '' || password == '') {
+        // Validacion fallo 
         Swal.fire({
             icon: 'error',
             title: 'Error',
             text: 'Ambos campos son obligatorios',
           })
     } else {
-        Swal.fire({
-            icon: 'success',
-            title: 'Correcto',
-            text: 'Ambos campos fueron completados con exito',
-          })
+        // Ambos campos fueron completados correctamente
+
+        // Datos que se envian al servidor 
+       var datos = new FormData();
+       datos.append('usuario', usuario);
+       datos.append('password', password);
+       datos.append('accion', tipo);
+
+       // Creando el Llamado a Ajax
+       var xhr = new XMLHttpRequest();
+
+       // Abrir la conexcion 
+       xhr.open('POST', 'inc/modelos/modelo-admin.php', true);
+
+       // Retorno de datos 
+       xhr.onload = function(){
+           if(this.status === 200) {
+               console.log(JSON.parse(xhr.responseText))
+           }
+       }
+
+       // Enviar la peticion
+       xhr.send(datos);
+
     }
 }
